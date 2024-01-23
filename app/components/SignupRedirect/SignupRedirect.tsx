6ddb3d6d2af86e104dd7c04788ce103a2ffd2ec4
Brian Bassett <brianbsstt@gmail.com>
2024-01-23 10:33:46 -0500

    color issues

--- app/components/SignupRedirect/SignupRedirect.tsx
+++ app/components/SignupRedirect/SignupRedirect.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SignupRedirect({ currentPage }: { currentPage: string }) {
   return (
-    <div className="mx-auto container text-center pt-20">
+    <div className="mx-auto container text-center pt-20 bg-white">
       <div>Must be logged in to view {currentPage}!</div>
       <div>Please Login or Signup!</div>
     </div>
--- app/friends/page.tsx
+++ app/friends/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Page() {
   return (
-    <div className="container mx-auto text-center pt-20">
+    <div className="container mx-auto text-center pt-20 bg-white">
       You dont have any friends! Lol
     </div>
   );
